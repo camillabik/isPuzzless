3ff98815ca6b1e68b688fac7b715948181fad398
admin <k.bikmukhametova.stc@innopolis.ru>
2017-10-16 10:08:16 +0300

    корректировка

--- src/main/java/jaxb/userJaxb/ReaderJaxbUser.java
+++ src/main/java/jaxb/userJaxb/ReaderJaxbUser.java
@@ -1,6 +1,8 @@
 package jaxb.userJaxb;
 
 
+import pojo.User;
+
 import javax.xml.bind.JAXBContext;
 import javax.xml.bind.JAXBException;
 import javax.xml.bind.Unmarshaller;
@@ -8,13 +10,13 @@ import java.io.File;
 
 public class ReaderJaxbUser {
     public static void main(String[] args) throws JAXBException {
-//        File file = new File("C:\\Users\\admin\\IdeaProjects\\test1\\src\\puzzles\\jaxb\\user.xml");
-//        JAXBContext jaxbContext = JAXBContext.newInstance(User.class);
-//
-//        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
-//        User user = (User) jaxbUnmarshaller.unmarshal(file);
-//        System.out.println(user.getLogin());
-//        System.out.println(user.getName());
+        File file = new File("C:\\Users\\admin\\IdeaProjects\\test1\\src\\puzzles\\jaxb\\user.xml");
+        JAXBContext jaxbContext = JAXBContext.newInstance(User.class);
+
+        Unmarshaller jaxbUnmarshaller = jaxbContext.createUnmarshaller();
+        User user = (User) jaxbUnmarshaller.unmarshal(file);
+        System.out.println(user.getLogin());
+        System.out.println(user.getName());
 
     }
 }
